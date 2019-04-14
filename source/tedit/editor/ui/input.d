4d73722a4dfbd09cc07d4c79320a60dc760773cf
Luís Ferreira <contact@lsferreira.net>
2019-04-14 22:39:31 +0100

    input: working on a custom input handler
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- source/tedit/editor/ui/input.d
+++ source/tedit/editor/ui/input.d
@@ -1,14 +1,17 @@
 module tedit.editor.ui.input;
 
 import aurorafw.core.input.events;
-import std.ascii;
+import aurorafw.core.input.keys;
 
-/*
+import std.ascii : isAlpha, toUpper;
+import std.conv : parse, to;
+
 KeyboardEvent translatePosixKeycode(string keyseq)
 {
-	if(isPrintable(keyseq[0]))
+	if(isAlpha(keyseq[0]))
 	{
-		
+		string ret = to!string(toUpper(keyseq[0]));
+		return KeyboardEvent(parse!Keycode(ret), InputModifier.None);
 	}
+	return KeyboardEvent(Keycode.Unknown, InputModifier.None);
 }
-*/
